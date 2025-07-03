dd7a4d8db555fe1a59bb603bf50ce3098501e9e6
Gedion Zewdu <gedeonzewdu@gmail.com>
2025-07-03 12:26:37 +0300

    Create build.gradle.kts

---
+++ app/src/app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    application
+    kotlin("jvm") version "1.9.10"
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-netty:2.3.0")
+    implementation("ch.qos.logback:logback-classic:1.4.6")
+}
